ae78d203fab7aa70e4ade9a638251ae2cced744e
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2018-04-01 17:38:35 +0800

    Applied go fmt

--- examples/sample.go
+++ examples/sample.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/hideshi/goawk"
 	"fmt"
-    "strconv"
-    "regexp"
+	"github.com/hideshi/goawk"
+	"regexp"
+	"strconv"
 )
 
 func Begin(app *goawk.App) {
@@ -15,24 +15,24 @@ func Begin(app *goawk.App) {
 func Action1(app *goawk.App) {
 	fmt.Println("Action1を実行します。")
 	fmt.Println("入力は１行ごとに処理され、文字列はフィールドセパレータで分割されます。")
-    for _, elem := range app.S[1:] {
-	    fmt.Printf("%#v\n", elem)
-        v, err := strconv.Atoi(elem)
-        if err != nil {
-            fmt.Println(err)
-        } else {
-            app.VI["sum"] = app.VI["sum"] + v
-        }
-    }
+	for _, elem := range app.S[1:] {
+		fmt.Printf("%#v\n", elem)
+		v, err := strconv.Atoi(elem)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			app.VI["sum"] = app.VI["sum"] + v
+		}
+	}
 }
 
 func Action2(app *goawk.App) {
 	fmt.Println("Action2を実行します。")
-    pattern := ".*20.*"
-    matched, _ := regexp.MatchString(pattern, app.S[0])
-    if matched == true {
-        fmt.Printf("%sが%sのパターンにマッチしました。\n", app.S[0], pattern)
-    }
+	pattern := ".*20.*"
+	matched, _ := regexp.MatchString(pattern, app.S[0])
+	if matched == true {
+		fmt.Printf("%sが%sのパターンにマッチしました。\n", app.S[0], pattern)
+	}
 }
 
 func End(app *goawk.App) {
